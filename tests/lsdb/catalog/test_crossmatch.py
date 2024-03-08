21fa65801391a89071ab0c7597f29023dc5a261a
delucchi-cmu <delucchi@andrew.cmu.edu>
2024-03-08 11:12:07 -0500

    Fix data and column names for kdtree unit test.

--- tests/lsdb/catalog/test_crossmatch.py
+++ tests/lsdb/catalog/test_crossmatch.py
@@ -8,7 +8,7 @@ def test_kdtree_crossmatch(small_sky_catalog_cloud, small_sky_xmatch_catalog_clo
         ).compute()
     assert len(xmatched) == len(xmatch_correct_cloud)
     for _, correct_row in xmatch_correct_cloud.iterrows():
-        assert correct_row["ss_id"] in xmatched["id_small_sky"].values
-        xmatch_row = xmatched[xmatched["id_small_sky"] == correct_row["ss_id"]]
+        assert correct_row["small_sky_id"] in xmatched["id_small_sky"].values
+        xmatch_row = xmatched[xmatched["id_small_sky"] == correct_row["small_sky_id"]]
         assert xmatch_row["id_small_sky_xmatch"].values == correct_row["xmatch_id"]
-        assert xmatch_row["_DIST"].values == pytest.approx(correct_row["dist"])
+        assert xmatch_row["_dist_arcsec"].values == pytest.approx(correct_row["dist_arcsec"])
