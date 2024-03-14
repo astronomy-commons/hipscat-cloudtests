2a35ae8fbfb78a0783e000e900820c0733561a3f
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2024-03-14 11:48:08 -0400

    Update plotting method testing. (#14)

--- tests/hipscat/inspection/test_visualize_catalog_cloud.py
+++ tests/hipscat/inspection/test_visualize_catalog_cloud.py
@@ -1,9 +1,18 @@
+import healpy as hp
 from hipscat.catalog import Catalog
 from hipscat.inspection import plot_pixels, plot_points
 
+# pylint: disable=no-member
 
-def test_generate_map_order1(small_sky_dir_cloud, example_cloud_storage_options):
+
+def test_generate_map_order1(small_sky_dir_cloud, example_cloud_storage_options, mocker):
     """Basic test that map data can be generated (does not test that a plot is rendered)"""
     cat = Catalog.read_from_hipscat(small_sky_dir_cloud, storage_options=example_cloud_storage_options)
-    plot_pixels(cat, draw_map=False)
-    plot_points(cat, draw_map=False)
+
+    mocker.patch("healpy.mollview")
+    plot_pixels(cat)
+    hp.mollview.assert_called_once()
+
+    hp.mollview.reset_mock()
+    plot_points(cat)
+    hp.mollview.assert_called_once()
