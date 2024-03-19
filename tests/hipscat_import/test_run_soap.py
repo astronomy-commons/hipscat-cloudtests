ef5e5fa1f1d3a0d0ab97ab03124ad93500bebfa9
delucchi-cmu <delucchi@andrew.cmu.edu>
2024-03-19 11:23:00 -0400

    SOAP test

---
+++ tests/hipscat_import/test_run_soap.py
@@ -0,0 +1,136 @@
+import os
+
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pytest
+from hipscat.catalog.association_catalog.association_catalog import AssociationCatalog
+
+import hipscat_import.soap.run_soap as runner
+from hipscat_import.soap.arguments import SoapArguments
+
+from hipscat_cloudtests import TempCloudDirectory, assert_text_file_matches
+from hipscat.io.file_io import read_parquet_metadata
+
+
+@pytest.mark.dask
+def test_object_to_self_write_to_cloud(
+    dask_client,
+    tmp_path,
+    tmp_dir_cloud,
+    small_sky_dir_local,
+    small_sky_order1_dir_local,
+    example_cloud_storage_options,
+):
+    """Test creating association between object catalogs.
+
+    using:
+    - local origin catalogs
+    - writing to CLOUD
+
+    First test creates leaf files, the second test does not (exercies different write calls).
+    """
+    with TempCloudDirectory(tmp_dir_cloud, "run_soap", example_cloud_storage_options) as temp_path:
+        small_sky_soap_args = SoapArguments(
+            object_catalog_dir=small_sky_dir_local,
+            object_id_column="id",
+            source_catalog_dir=small_sky_order1_dir_local,
+            source_object_id_column="id",
+            source_id_column="id",
+            write_leaf_files=True,
+            output_artifact_name="small_sky_to_order1",
+            output_path=temp_path,
+            output_storage_options=example_cloud_storage_options,
+            progress_bar=False,
+            tmp_dir=tmp_path,
+        )
+        runner.run(small_sky_soap_args, dask_client)
+
+        ## Check that the association data can be parsed as a valid association catalog.
+        catalog = AssociationCatalog.read_from_hipscat(
+            small_sky_soap_args.catalog_path, storage_options=example_cloud_storage_options
+        )
+        assert catalog.on_disk
+        assert catalog.catalog_path == small_sky_soap_args.catalog_path
+        assert len(catalog.get_join_pixels()) == 4
+        assert catalog.catalog_info.total_rows == 131
+        assert catalog.catalog_info.contains_leaf_files
+
+        parquet_file_name = os.path.join(
+            small_sky_soap_args.catalog_path, "Norder=0", "Dir=0", "Npix=11.parquet"
+        )
+        parquet_file_metadata = read_parquet_metadata(
+            parquet_file_name, storage_options=example_cloud_storage_options
+        )
+        assert parquet_file_metadata.num_row_groups == 4
+        assert parquet_file_metadata.num_rows == 131
+        assert parquet_file_metadata.num_columns == 8
+
+        small_sky_soap_args = SoapArguments(
+            object_catalog_dir=small_sky_dir_local,
+            object_id_column="id",
+            source_catalog_dir=small_sky_order1_dir_local,
+            source_object_id_column="id",
+            source_id_column="id",
+            write_leaf_files=False,
+            output_artifact_name="small_sky_to_order1_soft",
+            output_path=temp_path,
+            output_storage_options=example_cloud_storage_options,
+            progress_bar=False,
+            tmp_dir=tmp_path,
+        )
+        runner.run(small_sky_soap_args, dask_client)
+
+        ## Check that the association data can be parsed as a valid association catalog.
+        catalog = AssociationCatalog.read_from_hipscat(
+            small_sky_soap_args.catalog_path, storage_options=example_cloud_storage_options
+        )
+        assert catalog.on_disk
+        assert catalog.catalog_path == small_sky_soap_args.catalog_path
+        assert len(catalog.get_join_pixels()) == 4
+        assert catalog.catalog_info.total_rows == 131
+        assert not catalog.catalog_info.contains_leaf_files
+
+
+@pytest.mark.dask
+def test_object_to_self_read_from_cloud(
+    dask_client, tmp_path, small_sky_dir_cloud, small_sky_order1_dir_cloud, example_cloud_storage_options
+):
+    """Test creating association between object catalogs.
+
+    using:
+    - CLOUD origin catalogs
+    - writing to local tmp
+
+    First test creates leaf files, the second test does not (exercies different write calls).
+    """
+    small_sky_soap_args = SoapArguments(
+        object_catalog_dir=small_sky_dir_cloud,
+        object_id_column="id",
+        object_storage_options=example_cloud_storage_options,
+        source_catalog_dir=small_sky_order1_dir_cloud,
+        source_object_id_column="id",
+        source_id_column="id",
+        source_storage_options=example_cloud_storage_options,
+        write_leaf_files=True,
+        output_artifact_name="small_sky_to_order1",
+        output_path=tmp_path,
+        progress_bar=False,
+        tmp_dir=tmp_path,
+    )
+    runner.run(small_sky_soap_args, dask_client)
+
+    ## Check that the association data can be parsed as a valid association catalog.
+    catalog = AssociationCatalog.read_from_hipscat(small_sky_soap_args.catalog_path)
+    assert catalog.on_disk
+    assert catalog.catalog_path == small_sky_soap_args.catalog_path
+    assert len(catalog.get_join_pixels()) == 4
+    assert catalog.catalog_info.total_rows == 131
+    assert catalog.catalog_info.contains_leaf_files
+
+    parquet_file_name = os.path.join(small_sky_soap_args.catalog_path, "Norder=0", "Dir=0", "Npix=11.parquet")
+    parquet_file_metadata = read_parquet_metadata(
+        parquet_file_name, storage_options=example_cloud_storage_options
+    )
+    assert parquet_file_metadata.num_row_groups == 4
+    assert parquet_file_metadata.num_rows == 131
+    assert parquet_file_metadata.num_columns == 8
